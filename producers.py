9d1c612b9e7931452f6c941955d749bebba43187
Benjamin Ramsay <bramsay3@jhu.edu>
2018-06-13 13:49:50 -0400

    got rid of cmd line arguments

--- producers.py
+++ producers.py
@@ -3,9 +3,6 @@ import socket
 import sys
 
 if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        sys.stderr.write('Usage: %s <bootstrap-brokers>\n' % sys.argv[0])
-        sys.exit(1)
 
     broker = 'localhost:9092'
     topic_phone = 'phone_loc'
