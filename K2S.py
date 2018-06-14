2af837e3e309b4f7b85f2d0dad59d1bcf7fc1157
Benjamin Ramsay <bramsay3@jhu.edu>
2018-06-13 21:50:16 -0400

    fixed some indent errors

--- K2S.py
+++ K2S.py
@@ -7,19 +7,19 @@ from pyspark.streaming.kafka import KafkaUtils
 sc = SparkContext(master, appName)
 ssc = StreamingContext(sc, 1)
 
-directKafkaStream = KafkaUtils.createDirectStream(ssc, 'phone_loc', {"bootstrap.servers": localhost:9092})
+directKafkaStream = KafkaUtils.createDirectStream(ssc, 'phone_loc', {"bootstrap.servers": 'localhost:9092'})
 
- offsetRanges = []
+offsetRanges = []
 
- def storeOffsetRanges(rdd):
-     global offsetRanges
-     offsetRanges = rdd.offsetRanges()
-     return rdd
+def storeOffsetRanges(rdd):
+    global offsetRanges
+    offsetRanges = rdd.offsetRanges()
+    return rdd
 
- def printOffsetRanges(rdd):
-     for o in offsetRanges:
-         print "%s %s %s %s" % (o.topic, o.partition, o.fromOffset, o.untilOffset)
+def printOffsetRanges(rdd):
+    for o in offsetRanges:
+    print "%s %s %s %s" % (o.topic, o.partition, o.fromOffset, o.untilOffset)
 
- directKafkaStream \
-     .transform(storeOffsetRanges) \
-     .foreachRDD(printOffsetRanges)
+directKafkaStream \
+ .transform(storeOffsetRanges) \
+ .foreachRDD(printOffsetRanges)
