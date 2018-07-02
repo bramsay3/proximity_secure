b7ca5a2445f705dcddc3c8b820da99a208ab3421
Ubuntu <ubuntu@ip-10-0-0-7.ec2.internal>
2018-07-02 13:10:21 +0000

    created basic table display

--- K2S_add.py
+++ K2S_add.py
@@ -28,31 +28,14 @@ ssc = StreamingContext(sc, 2)
 
 dstream_combo = KafkaUtils.createDirectStream(ssc, ['COMBO'], {"bootstrap.servers": 'localhost:9092'})
 
-#rdd = sc.parallelize([{"user_id": 9, "transactions_timestamp": "2019-07-14 22:00:00", "distance": 8109.8952838063815, "phone_loc": "{\"lat\":43.10041624519112,\"lng\":-104.52802039617467}", "TRANSACTION_LOC": "{\"lng\":39.83744810841391,\"lat\":-103.22536502244108}", "PHONE_TIMESTAMP": "2019-02-14 17:00:00"}])
-
-#rdd = sc.parallelize([{'user_id': 3, 'transactions_timestamp': '2019-06-04 22:00:00', 'distance': 461.7505633584639, 'phone_loc': {"lat":40.602087555124854,"lng":-108.76210017318485}, 'transaction_loc': {"lat":39.550158835808006,"lng":-102.08468949015811}, 'phone_timestamp': '2019-02-14 17:00:00'}])
-
-#rdd = sc.parallelize([{'word': 'me','count':5}])
-#print(rdd.take(1))
-#rdd.saveToCassandra('users','locations')
-#print(rdd.take(1))
 
 json_combo  = dstream_combo.map(lambda x: json.loads(x[1]))
+json_combo.pprint(100)
 json_dist = json_combo.map(lambda data:distance(data))
-#json_dist.foreachRDD(lambda dic:dic.saveToCassandra('users','locations'))
 
-#print(sc.cassandraTable("users","locations").select("phone_loc").take(3))
+
 json_dist.pprint(100)
 json_dist.saveToCassandra('users','locations')
-
-#json_list = json_dist.foreachRDD(lambda rdd: rdd.collect())
-
-#for user in json_list:
-#    manager.insert(user)
-
-
-
-
 
 
 def distance(json_data, miles=True):
@@ -65,7 +48,7 @@ def distance(json_data, miles=True):
         return [lng,lat]
 
     loc_1 = extract_coords('PHONE_LOC')
-    loc_2 = extract_coords('TRANSACTION_LOC')
+    loc_2 = extract_coords('TRANS_LOC')
     distance = haversine(loc_1, loc_2, miles=miles)
 
     json_data['distance'] = distance
--- flask/flask_app/views.py
+++ flask/flask_app/views.py
@@ -1,18 +1,33 @@
 from flask import render_template
 from flask_app import app
-from flask_cassandra import CassandraCluster
+from cassandra.cluster import Cluster
+from cassandra.query import *
+from flask_table import Table, Col
 
 
-cassandra = CassandraCluster()
-app.config['CASSANDRA_NODES'] = ['ec2-18-233-215-146.compute-1.amazonaws.com','ec2-34-234-45-181.compute-1.amazonaws.com','ec2-52-20-107-122.compute-1.amazonaws.com']  # list of nodes
+class ItemTable(Table):
+           USER_ID = Col('user_ID')
+           TRANSACTIONS_TIMESTAMP = Col('trans_time')
+           PHONE_LOC = Col('phone_loc')
+           PHONE_TIMESTAMP = Col('phone_time')
+           TRANS_LOC = Col('trans_loc')
+           distance = Col('distance')
 
-@app.route("/cassandra_test")
-def cassandra_test():
-       session = cassandra.connect()
-       session.set_keyspace("users")
-       cql = "SELECT * FROM users.locations"
-       r = session.execute(cql)
-       return str(r[:2])
+@app.route("/table")
+def table():
+       cluster = Cluster(['ec2-18-233-215-146.compute-1.amazonaws.com'])
+       session = cluster.connect()
+#       session = cluster.connect(['ec2-18-233-215-146.compute-1.amazonaws.com','ec2-34-234-45-181.compute-1.amazonaws.com','ec2-52-20-107-122.compute-1.amazonaws.com'])
+       session.row_factory =  named_tuple_factory
+       cql = "SELECT * FROM users.locations LIMIT 10"
+       results = session.execute(cql)
+       
+
+       header = ['User ID','Transaction Location','Transaction Time','Phone Location','Phone Time','Distance(miles)']
+           
+       return render_template("table.html", header=header, rows = results)
+
+
 
 @app.route('/')
 @app.route('/index')
