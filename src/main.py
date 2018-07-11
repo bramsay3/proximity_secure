bfe21dcbb2246040053d86269ce6cb7009f5dc2e
Benjamin Ramsay <bramsay3@jhu.edu>
2018-07-11 18:54:15 -0400

    removed a merge conflict from main

--- src/main.py
+++ src/main.py
@@ -26,7 +26,6 @@ while True:
     print('gen time: ',time.time()-start1)
     start2 = time.time()
     phone_prod.start_producing(phone)
-<<<<<<< HEAD
     print('prod phone time: ',time.time()-start2)
     start3 = time.time()
     trans_prod.start_producing(trans)
@@ -34,20 +33,6 @@ while True:
     end = time.time()  
     duration = end - start1
     print("Total time:",duration, "seconds")
-=======
-    end = time.time()  
-    duration = end - start
-    wait = 60 - duration
-    prod_start = time.time()
 
-    trans_len = len(trans)
-    for i in range(math.floor(wait/2))
-
-    trans_prod.start_producing(trans)
-    if duration > 60:
-        raise Exception("Data generation exceeded 1 minute")
-    print("Data generation time: ",duration, "seconds")
-    print("Waiting ",wait," seconds till next generation")
->>>>>>> 58ab06fa7f7f28884227a1205132b3bcf355f0cb
     phone_prod.prod.flush()
     trans_prod.prod.flush()
